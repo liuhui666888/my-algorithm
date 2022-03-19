630a383af469e3b8cadadda615045bc510a83c6b
liuhui666888 <343920368@qq.com>
2022-03-19 22:39:03 +0800

    新增求树的最长直径

--- src/main/java/com/algorithm/tree/TreeAlgorithm.java
+++ src/main/java/com/algorithm/tree/TreeAlgorithm.java
@@ -9,23 +9,50 @@ public class TreeAlgorithm {
 
     /**
      * Description:求树的最大深度
-     * @param root
-     * @return: int
      * @Author: liuhui
      * @Date: 2022/3/19
      **/
-    public int Solution104(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        // 利用定义，计算左右子树的最大深度
-        int leftMax = Solution104(root.left);
-        int rightMax = Solution104(root.right);
-        // 整棵树的最大深度等于左右子树的最大深度取最大值，
-        // 然后再加上根节点自己
-        int res = Math.max(leftMax, rightMax) + 1;
-        return res;
+    public class Solution104{
+        public int maxDepth(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+            // 利用定义，计算左右子树的最大深度
+            int leftMax = maxDepth(root.left);
+            int rightMax = maxDepth(root.right);
+            // 整棵树的最大深度等于左右子树的最大深度取最大值，
+            // 然后再加上根节点自己
+            int res = Math.max(leftMax, rightMax) + 1;
+            return res;
 
+        }
+    }
+
+    /**
+     * Description: 求最长直径（例如：左边3个子节点，右边2个，最大直径为5）
+     * @Author: liuhui
+     * @Date: 2022/3/19
+     **/
+    public class Solution543{
+        int max = 0;
+        public int diameterOfBinaryTree(TreeNode root) {
+            maxDept(root);
+            return max;
+        }
+        public int maxDept(TreeNode root){
+            if(root == null){
+                //这个地方返回0，不能返回max，因为max是全局的
+                //会发生左子树修改后，右子树返回同样值的问题
+                return 0;
+            }
+            int leftCount = maxDept(root.left);
+            int rightCount = maxDept(root.right);
+            //将最大深度记录到临时变量
+            int myMax = leftCount+rightCount;
+            //比较最大的赋值，此处注意：如果直接返回临时变量会存在少了最后一个子节点统计的问题
+            max = Math.max(max,myMax);
+            return Math.max(leftCount,rightCount)+1;
+        }
     }
 
 
